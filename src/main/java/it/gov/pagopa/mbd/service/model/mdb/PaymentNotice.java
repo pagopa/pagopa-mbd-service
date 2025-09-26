1941f3fcd234ce8ea01e554635a097b8b5b46104
giomella <giomella@emeal.nttdata.com>
2025-09-26 17:07:55 +0200

    [PIDM-878] improve API documentation

--- src/main/java/it/gov/pagopa/mbd/service/model/mdb/PaymentNotice.java
+++ src/main/java/it/gov/pagopa/mbd/service/model/mdb/PaymentNotice.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.mbd.service.model.mdb;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,13 +13,31 @@ import lombok.NoArgsConstructor;
 @Builder
 public class PaymentNotice {
 
-  @NotBlank private String firstName;
-  @NotBlank private String lastName;
-  @NotBlank private String fiscalCode;
-  @NotBlank private String email;
-  @NotNull private Long amount;
-  @NotBlank private String province;
+  @Schema(example = "Mario", description = "Debtor name.")
+  @NotBlank
+  private String firstName;
 
+  @Schema(example = "Rossi", description = "Debtor surname.")
+  @NotBlank
+  private String lastName;
+
+  @Schema(example = "00000000000000000", description = "Debtor fiscal code.")
+  @NotBlank
+  private String fiscalCode;
+
+  @Schema(example = "mario.rossi@test.test", description = "Debtor mail.")
+  @NotBlank
+  private String email;
+
+  @Schema(example = "16", description = "MBD amount.")
+  @NotNull
+  private Long amount;
+
+  @Schema(example = "RM", description = "Debtor province.")
+  @NotBlank
+  private String province;
+
+  @Schema(description = "Document hash.")
   @Size(min = 44, max = 44)
   private String documentHash;
 }
